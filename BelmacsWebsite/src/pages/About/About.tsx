7a384a0686c9f17f3b9647a4b2f296db3822525a
quik214 <tzejunw@gmail.com>
2024-05-15 16:58:05 +0800

    Update folder structure for About

--- BelmacsWebsite/src/pages/About/About.tsx
+++ BelmacsWebsite/src/pages/About/About.tsx
@@ -1,43 +1,13 @@
-import AboutHero from "../../components/AboutHero/AboutHero"
+import AboutHero from "../../components/About/AboutHero/AboutHero"
 import "./About.css"
 
 export default function () {
   return (
     <div>
       <AboutHero />
-      <p>
-        Hello there dolor sit amet, consectetur adipiscing elit. Sed ac felis et
-        eros dictum faucibus. Nullam eget nunc eu nisl facilisis suscipit vel
-        non nunc. In hac habitasse platea dictumst. Nulla facilisi. Fusce sit
-        amet erat justo. Morbi lacinia, velit eget scelerisque laoreet, nulla
-        risus sollicitudin odio, eget fermentum urna purus vel justo. Vivamus at
-        odio quis magna ultricies convallis. Sed quis lectus nunc. Cras vehicula
-        ex eget nulla sollicitudin, eu suscipit neque auctor. Ut sit amet eros
-        eu tellus vulputate accumsan. Sed in dignissim libero. Ut vestibulum
-        diam eu libero tristique, vel suscipit nulla sodales.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac felis et
-        eros dictum faucibus. Nullam eget nunc eu nisl facilisis suscipit vel
-        non nunc. In hac habitasse platea dictumst. Nulla facilisi. Fusce sit
-        amet erat justo. Morbi lacinia, velit eget scelerisque laoreet, nulla
-        risus sollicitudin odio, eget fermentum urna purus vel justo. Vivamus at
-        odio quis magna ultricies convallis. Sed quis lectus nunc. Cras vehicula
-        ex eget nulla sollicitudin, eu suscipit neque auctor. Ut sit amet eros
-        eu tellus vulputate accumsan. Sed in dignissim libero. Ut vestibulum
-        diam eu libero tristique, vel suscipit nulla sodales.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac felis et
-        eros dictum faucibus. Nullam eget nunc eu nisl facilisis suscipit vel
-        non nunc. In hac habitasse platea dictumst. Nulla facilisi. Fusce sit
-        amet erat justo. Morbi lacinia, velit eget scelerisque laoreet, nulla
-        risus sollicitudin odio, eget fermentum urna purus vel justo. Vivamus at
-        odio quis magna ultricies convallis. Sed quis lectus nunc. Cras vehicula
-        ex eget nulla sollicitudin, eu suscipit neque auctor. Ut sit amet eros
-        eu tellus vulputate accumsan. Sed in dignissim libero. Ut vestibulum
-        diam eu libero tristique, vel suscipit nulla sodales.
-      </p>
+      <div className="container">
+        
+      </div>
     </div>
   );
 }
