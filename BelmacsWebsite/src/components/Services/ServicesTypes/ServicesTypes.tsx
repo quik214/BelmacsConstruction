71ef6be0e91ef9b8586acd126eca2f5d1967f25c
Kiwinoob <Kiwi1@live.com.sg>
2024-06-02 18:35:07 +0800

    added servicesType(half done)

---
+++ BelmacsWebsite/src/components/Services/ServicesTypes/ServicesTypes.tsx
@@ -0,0 +1,105 @@
+import "../../../assets/fonts.css";
+import "./ServicesTypes.css";
+import "./ServicesTypes-media.css";
+
+import { useNavigate } from "react-router-dom";
+
+/* Hero Images to pass */
+/* Only Residential image is good, the rest needs to change */
+
+export type servicesTypeItem = {
+  title?: string;
+  image?: string;
+  css?: string;
+};
+
+export const servicesTypes: servicesTypeItem[] = [
+  {
+    title: "Air Conditioning and Mechanical Ventilation",
+    image: "",
+    css: "Air Conditioning and Mechanical Ventilation",
+  },
+  {
+    title: "Plumbing, Sanitary and Gas",
+    image: "",
+    css: "Plumbing Sanitary and Gas",
+  },
+  {
+    title: "Fire Protection",
+    image: "",
+    css: "Fire Protection",
+  },
+  {
+    title: "Swimming Pool Filtration",
+    image: "",
+    css: "Swimming Pool Filtration",
+  },
+  {
+    title: "Electrical and Security",
+    image: "",
+    css: "Electrical and Security",
+  },
+  {
+    title: "Lift and Escalator",
+    image: "",
+    css: "Lift and Escalator",
+  },
+  {
+    title: "Building Management System",
+    image: "",
+    css: "Building Management System",
+  },
+  {
+    title: "Feasibility Studies",
+    image: "",
+    css: "Feasibility Studies",
+  },
+  {
+    title: "Testing and Commissioning",
+    image: "",
+    css: "Testing and Commissioning",
+  },
+  {
+    title: "Authority and Submission",
+    image: "",
+    css: "Authority and Submission",
+  },
+  {
+    title: "Vetting of Tenancy M&E Design",
+    image: "",
+    css: "Vetting of Tenancy ME Design",
+  },
+];
+
+export default function ProjectsTypes() {
+  return (
+    <div className="category">
+      <p className="category-header">Services</p>
+      <p className="category-desc">
+        We are a leading provider of integrated automation and control
+        solutions. Specializing in customized automation systems, the company
+        serves diverse industries such as marine, oil and gas, and industrial
+        sectors. Our offerings include advanced engineering services, innovative
+        automation products, and comprehensive system integration solutions.
+        With a focus on enhancing operational efficiency, we ensure safety and
+        deliver cutting-edge technology tailored to meet specific client needs.
+        With a commitment to quality and reliability, Belmacs stands out as a
+        trusted partner for complex automation and control projects.
+      </p>
+
+      <div className="grid-container">
+        {servicesTypes.map((type) => (
+          <div
+            key={type.title} // Assuming title is unique for each category
+            // make sure css works correctly
+            className={`grid-item ${type.css
+              ?.toLowerCase()
+              .replace(/ /g, "-")}`}
+          >
+            <div className="overlay">{type.title}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- BelmacsWebsite/src/pages/Services/Services.tsx
+++ BelmacsWebsite/src/pages/Services/Services.tsx
@@ -1,3 +1,4 @@
+import ServicesTypes from "../../components/Services/ServicesTypes/ServicesTypes";
 import Hero from "../../components/Hero/Hero";
 
 import ServicesHeroImage from "../../assets/Hero/Services.jpg";
@@ -6,6 +7,9 @@ export default function Services() {
   return (
     <div>
       <Hero imageUrl={ServicesHeroImage} heroText="Our Services" />
+      <div className="container">
+        <ServicesTypes />
+      </div>
     </div>
   );
 }
