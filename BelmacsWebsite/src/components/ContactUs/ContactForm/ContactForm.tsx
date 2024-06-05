f4bfabf8d6faa804d24241311b7f4f7e0b6f3ea5
quik214 <tzejunw@gmail.com>
2024-06-05 19:50:04 +0800

    Revert changes of ContactForm

--- BelmacsWebsite/src/components/ContactUs/ContactForm/ContactForm.tsx
+++ BelmacsWebsite/src/components/ContactUs/ContactForm/ContactForm.tsx
@@ -19,19 +19,12 @@ const ContactForm: React.FC = () => {
 
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (formRef.current) {
-      // hCaptcha validation
-      const token = (window as any).hcaptcha.getResponse();
-      if (!token) {
-        setResultMessage('Please complete the hCaptcha challenge');
-        return;
-      }
 
+    if (formRef.current) {
       // Create form data and append hidden fields
       const formData = new FormData(formRef.current);
       formData.append('access_key', '7bfd764b-90c7-4931-bbfc-c68f7948ac86');
       formData.append('subject', 'Someone dropped you a message from ContactUsForm');
-      formData.append('h-captcha-response', token);
 
       // Form submission logic
       fetch('https://api.web3forms.com/submit', {
@@ -43,15 +36,12 @@ const ContactForm: React.FC = () => {
             setResultMessage('Your message has been sent successfully!');
           } else {
             console.error('Form submission error:', response.statusText);
-            setResultMessage('Form submission error: ' + response.statusText);
           }
         })
         .catch((error) => setResultMessage('Form submission error: ' + error.message));
 
       // Reset the form
       formRef.current.reset();
-      // Reset hCaptcha
-      (window as any).hcaptcha.reset();
     } else {
       console.error('Form reference is null');
       setResultMessage('Form reference is null');
@@ -63,17 +53,17 @@ const ContactForm: React.FC = () => {
       <fieldset>
         <legend>Contact Us</legend>
         <input type="checkbox" name="botcheck" style={{   display: 'none' }} />
-        
+
         <div>
           <label htmlFor="name">Full Name</label><br />
           <input type="text" name="name" id="name" placeholder="Lee Hsien Looong" required /><br /><br />
         </div>
-        
+
         <div>
           <label htmlFor="email">Email Address</label><br />
           <input type="email" name="email" id="email" placeholder="you@company.com" required /><br /><br />
         </div>
-        
+
         <div>
           <label htmlFor="enquiry-type">Enquiry Type</label><br />
           <select name="enquiry-type" id="enquiry-type" required>
@@ -84,13 +74,11 @@ const ContactForm: React.FC = () => {
             <option value="feedback">Feedback</option>
           </select><br /><br />
         </div>
-        
+
         <div>
           <label htmlFor="message">Message</label><br />
           <textarea rows={5} name="message" id="message" placeholder="Your Message" required></textarea><br /><br />
         </div>
-
-        <div className="h-captcha" data-sitekey="00c8b5f3-b304-4395-a249-05da8ae57c27"></div>
 
         <button type="submit">Send Message</button>
 
