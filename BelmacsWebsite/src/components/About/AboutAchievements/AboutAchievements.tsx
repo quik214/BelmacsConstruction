cb03de3e950fd1204e29e963f8f37c66cea74ed2
leong2699 <hymleonggoh@hotmail.com>
2024-06-21 16:35:12 +0800

    Fix bugs for Achievements

--- BelmacsWebsite/src/components/About/AboutAchievements/AboutAchievements.tsx
+++ BelmacsWebsite/src/components/About/AboutAchievements/AboutAchievements.tsx
@@ -6,13 +6,12 @@ import {
   DocumentData,
 } from "firebase/firestore";
 import { db } from "../../../firebase";
-
 import "./AboutAchievements.css";
 import "./AboutAchievements-media.css";
-
 import buildingLogo from "../../../assets/Icons/AboutAchievements/building-office.svg";
 import hatLogo from "../../../assets/Icons/AboutAchievements/hard-hat.svg";
 import certificateLogo from "../../../assets/Icons/AboutAchievements/certificate.svg";
+import calendarLogo from "../../../assets/Icons/AboutAchievements/calendar-check.svg";
 
 // for particles.js
 import Particles, { initParticlesEngine } from "@tsparticles/react";
@@ -27,20 +26,17 @@ interface Award {
   id: string;
   title: string;
 }
-
 interface Project {
   id: string;
   type: string;
   awards: Award[];
 }
-
 const AboutAchievements = () => {
   const [totalAwards, setTotalAwards] = useState(0);
   const [totalProjects, setTotalProjects] = useState(0);
   const [yearsOfExperience, setYearsOfExperience] = useState(0);
   const [inView, setInView] = useState(false);
   const achievementsRef = useRef<HTMLDivElement | null>(null);
-
   useEffect(() => {
     const fetchDataFromFirestore = async () => {
       try {
@@ -55,27 +51,21 @@ const AboutAchievements = () => {
         let awardsCount = 0;
         let projectsCount = 0;
         const allProjects: Project[] = [];
-
         for (const type of projectTypes) {
           const projectsCollectionRef = collection(db, `${type}-projects`);
           const querySnapshot = await getDocs(projectsCollectionRef);
-
           projectsCount += querySnapshot.size;
-
           await Promise.all(
             querySnapshot.docs.map(
               async (doc: QueryDocumentSnapshot<DocumentData>) => {
                 const projectData = doc.data();
                 const awardsCollectionRef = collection(doc.ref, "awards");
                 const awardsSnapshot = await getDocs(awardsCollectionRef);
-
                 const awards = awardsSnapshot.docs.map((awardDoc) => ({
                   id: awardDoc.id,
                   title: awardDoc.data().title,
                 }));
-
                 awardsCount += awards.length;
-
                 allProjects.push({
                   id: doc.id,
                   type: type,
@@ -86,11 +76,9 @@ const AboutAchievements = () => {
             )
           );
         }
-
         const currentYear = new Date().getFullYear();
         const startYear = 1994;
         const experience = currentYear - startYear;
-
         setTotalAwards(awardsCount);
         setTotalProjects(projectsCount);
         setYearsOfExperience(experience);
@@ -98,23 +86,18 @@ const AboutAchievements = () => {
         console.error("Error fetching data: ", error);
       }
     };
-
     fetchDataFromFirestore();
   }, []);
-
   useEffect(() => {
     initParticlesEngine(async (engine) => {
       await loadSlim(engine);
     });
   }, []);
-
   const particlesLoaded = async (container?: Container): Promise<void> => {
     console.log(container);
   };
-
   const particlesOptions: ISourceOptions =
     particlesJSON as unknown as ISourceOptions;
-
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
@@ -125,18 +108,15 @@ const AboutAchievements = () => {
       },
       { threshold: 0.1 }
     );
-
     if (achievementsRef.current) {
       observer.observe(achievementsRef.current);
     }
-
     return () => {
       if (achievementsRef.current) {
         observer.unobserve(achievementsRef.current);
       }
     };
   }, []);
-
   return (
     <div className="achievements-container reveal" ref={achievementsRef}>
       <Particles
@@ -144,24 +124,27 @@ const AboutAchievements = () => {
         particlesLoaded={particlesLoaded}
         options={particlesOptions}
       />
-
       <div className="achievements-header">Our Achievements</div>
       <div className="achievements">
         <div className="circle-wrapper">
           <div className="achievement">
             <img className="icon" src={hatLogo} alt="Hard Hat Icon"></img>
             <div className="info">
-              <h3> {inView && <CountUp end={1994} duration={2} />}</h3>
+              <h3>
+                {" "}
+                {inView && <CountUp end={1994} duration={2} separator="" />}
+              </h3>
               <p>Year Established</p>
             </div>
           </div>
         </div>
         <div className="circle-wrapper">
           <div className="achievement">
-            <img className="icon" src={hatLogo} alt="Hard Hat Icon"></img>
+            <img className="icon" src={calendarLogo} alt="Calendar Icon"></img>
             <div className="info">
               <h3>
-                {inView && <CountUp end={yearsOfExperience} duration={3} />} years
+                {inView && <CountUp end={yearsOfExperience} duration={3} />}{" "}
+                years
               </h3>
               <p>of Experience</p>
             </div>
@@ -193,5 +176,4 @@ const AboutAchievements = () => {
     </div>
   );
 };
-
 export default AboutAchievements;
