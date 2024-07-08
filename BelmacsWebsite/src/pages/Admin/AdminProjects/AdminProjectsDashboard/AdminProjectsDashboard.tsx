2f0e1cab2f40b0813c35db722c4540d8b63ebdfe
Kiwinoob <Kiwi1@live.com.sg>
2024-07-08 16:03:32 +0800

    add tab for admin project dashboard

--- BelmacsWebsite/src/pages/Admin/AdminProjects/AdminProjectsDashboard/AdminProjectsDashboard.tsx
+++ BelmacsWebsite/src/pages/Admin/AdminProjects/AdminProjectsDashboard/AdminProjectsDashboard.tsx
@@ -1,13 +1,42 @@
-import "./AdminProjectsDashboard.css";
-
+import React, { useState } from "react";
 import AdminProjectsDashboard from "../../../../components/Admin/Projects/ProjectsDashboard/ProjectsDashboard";
 import AdminProjectsTypesDashboard from "../../../../components/Admin/Projects/ProjectsTypeDashboard/ProjectsTypesDashboard";
+import "./AdminProjectsDashboard.css";
+import "./AdminProjectsDashboard-media.css";
 
-export default function AdminDashboard() {
+const AdminDashboard: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<"projects" | "projectsTypes">(
+    "projects"
+  );
+
+  const handleTabClick = (tab: "projects" | "projectsTypes") => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="admin-container">
-      <AdminProjectsDashboard />
-      <AdminProjectsTypesDashboard />
+      <div className="tabs">
+        <div
+          className={`tab-button ${activeTab === "projects" ? "active" : ""}`}
+          onClick={() => handleTabClick("projects")}
+        >
+          Projects
+        </div>
+        <div
+          className={`tab-button ${
+            activeTab === "projectsTypes" ? "active" : ""
+          }`}
+          onClick={() => handleTabClick("projectsTypes")}
+        >
+          Projects Category Image
+        </div>
+      </div>
+      <div className="tab-content">
+        {activeTab === "projects" && <AdminProjectsDashboard />}
+        {activeTab === "projectsTypes" && <AdminProjectsTypesDashboard />}
+      </div>
     </div>
   );
-}
+};
+
+export default AdminDashboard;
