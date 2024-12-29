dd2c0d958bf297b7ba733288b05a63040595c565
tzejunw <68325502+quik214@users.noreply.github.com>
2024-12-29 15:00:07 +0800

    Removed API Key

--- BelmacsWebsite/src/firebase.ts
+++ BelmacsWebsite/src/firebase.ts
@@ -10,16 +10,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-// DO NOT CHANGE
-const firebaseConfig = {
-  apiKey: "AIzaSyC_BCm9geOD8vanElhZ5D8tqn5tI2cJ8T8",
-  authDomain: "belmacs1994.firebaseapp.com",
-  projectId: "belmacs1994",
-  storageBucket: "belmacs1994.appspot.com",
-  messagingSenderId: "151567641645",
-  appId: "1:151567641645:web:f45f9265503efb6bc8dca4",
-  measurementId: "G-FZXFG5T9KE",
-};
+// PASTE API KEY HERE
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
